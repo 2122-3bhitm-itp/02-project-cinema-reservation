20158c7087003f77c9ba68748d9c7e5311874d24
P-Klose <ipad@klose.at>
2022-03-28 19:20:42 +0200

    added entity classes

--- cinema-reservation-code/src/main/java/at/htl/cinemareservation/model/Ticket.java
+++ cinema-reservation-code/src/main/java/at/htl/cinemareservation/model/Ticket.java
@@ -1,4 +1,23 @@
 package at.htl.cinemareservation.model;
 
 public class Ticket {
+    private Seat seat;
+    private final Presentation presentation;
+
+    public Ticket(Seat seat, Presentation presentation) {
+        this.seat = seat;
+        this.presentation = presentation;
+    }
+
+    public Seat getSeat() {
+        return seat;
+    }
+
+    public void setSeat(Seat seat) {
+        this.seat = seat;
+    }
+
+    public Presentation getPresentation() {
+        return presentation;
+    }
 }
