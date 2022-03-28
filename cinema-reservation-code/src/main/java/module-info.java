83829abd5c592dfa19cfcb97057014d6e7e55399
P-Klose <ipad@klose.at>
2022-03-28 17:51:29 +0200

    sql to module-info added

--- cinema-reservation-code/src/main/java/module-info.java
+++ cinema-reservation-code/src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module at.htl.cinemareservation {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.sql;
 
     opens at.htl.cinemareservation to javafx.fxml;
     exports at.htl.cinemareservation;
