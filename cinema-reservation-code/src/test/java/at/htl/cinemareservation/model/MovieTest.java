05f48772d602ace8dbadee199d7dc405f06e42b2
P-Klose <ipad@klose.at>
2022-03-31 10:24:54 +0200

    added first Tests

--- cinema-reservation-code/src/test/java/at/htl/cinemareservation/model/MovieTest.java
+++ cinema-reservation-code/src/test/java/at/htl/cinemareservation/model/MovieTest.java
@@ -10,11 +10,14 @@ class MovieTest {
     @Test
     void createMovie() {
         //arrange
-        Movie batman = new Movie(220,"Batman","Action",22.22);
+        Movie batman = new Movie(150,"Batman","Action",22.22);
         //act
         batman.setPrice(17.99);
         //assert
         assertThat(batman.getPrice()).isEqualTo(17.99);
+        assertThat(batman.getGenre()).isEqualTo("Action");
+        assertThat(batman.getLength()).isEqualTo(150);
+        assertThat(batman.getTitle()).isEqualTo("Batman");
     }
 
 }
