0ced304c0e3d6fd88bd074def63632847ae0dd2f
P-Klose <ipad@klose.at>
2022-04-07 11:13:13 +0200

    fixed module-info.java

--- cinema-reservation-code/src/main/java/module-info.java
+++ cinema-reservation-code/src/main/java/module-info.java
@@ -3,6 +3,7 @@ module at.htl.cinemareservation {
     requires javafx.fxml;
     requires java.sql;
     requires org.mybatis;
+    requires org.apache.derby.tools;
 
     opens at.htl.cinemareservation to javafx.fxml;
     exports at.htl.cinemareservation;
