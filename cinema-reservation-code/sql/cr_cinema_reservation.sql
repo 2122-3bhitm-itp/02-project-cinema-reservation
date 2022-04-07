4909300b4f500bf11f864f45b29f5618eb59f4d6
P-Klose <ipad@klose.at>
2022-04-07 12:03:52 +0200

    fix script

--- cinema-reservation-code/sql/cr_cinema_reservation.sql
+++ cinema-reservation-code/sql/cr_cinema_reservation.sql
@@ -9,6 +9,11 @@ CREATE TABLE room (
                     room_id INT NOT NULL PRIMARY KEY
 );
 
+CREATE TABLE presentation (
+                              presentation_id INT NOT NULL PRIMARY KEY,
+                              startTime TIMESTAMP not null
+);
+
 CREATE TABLE c_row (
                     row_id INT NOT NULL PRIMARY KEY,
                     room_id INT REFERENCES room(room_id),
@@ -25,11 +30,6 @@ CREATE TABLE ticket (
                     ticket_id INT NOT NULL PRIMARY KEY,
                     seat_id INT REFERENCES seat(seat_id),
                     presentation_id INT REFERENCES presentation(presentation_id)
-);
-
-CREATE TABLE presentation (
-                    presentation_id INT NOT NULL PRIMARY KEY,
-                    startTime TIMESTAMP not null
 );
 
 
