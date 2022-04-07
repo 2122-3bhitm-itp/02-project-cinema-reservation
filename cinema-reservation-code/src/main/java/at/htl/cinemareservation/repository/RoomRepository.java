df25b8082dbfab34fa9d1da3a3501dbf6fab2fa8
P-Klose <ipad@klose.at>
2022-04-07 11:29:37 +0200

    add Repository classes

--- cinema-reservation-code/src/main/java/at/htl/cinemareservation/repository/RoomRepository.java
+++ cinema-reservation-code/src/main/java/at/htl/cinemareservation/repository/RoomRepository.java
@@ -1,4 +1,33 @@
 package at.htl.cinemareservation.repository;
 
-public class RoomRepository {
+import at.htl.cinemareservation.controller.Persistent;
+import at.htl.cinemareservation.model.Room;
+
+import java.util.List;
+
+public class RoomRepository implements Persistent<Room> {
+    @Override
+    public void save(Room entity) {
+
+    }
+
+    @Override
+    public void insert(Room entity) {
+
+    }
+
+    @Override
+    public void delete(long id) {
+
+    }
+
+    @Override
+    public List<Room> findAll() {
+        return null;
+    }
+
+    @Override
+    public Room findById(long id) {
+        return null;
+    }
 }
--- cinema-reservation-code/src/main/java/at/htl/cinemareservation/repository/RowRepository.java
+++ cinema-reservation-code/src/main/java/at/htl/cinemareservation/repository/RowRepository.java
@@ -1,4 +1,33 @@
 package at.htl.cinemareservation.repository;
 
-public class RowRepository {
+import at.htl.cinemareservation.controller.Persistent;
+import at.htl.cinemareservation.model.Row;
+
+import java.util.List;
+
+public class RowRepository implements Persistent<Row> {
+    @Override
+    public void save(Row entity) {
+
+    }
+
+    @Override
+    public void insert(Row entity) {
+
+    }
+
+    @Override
+    public void delete(long id) {
+
+    }
+
+    @Override
+    public List<Row> findAll() {
+        return null;
+    }
+
+    @Override
+    public Row findById(long id) {
+        return null;
+    }
 }
